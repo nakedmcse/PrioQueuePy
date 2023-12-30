8741830e78aa56e1a97287fb7274752623a090c3
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-30 17:09:40 -0600

    first commit

---
+++ priorityQueue.py
@@ -0,0 +1,39 @@
+# Priority Queue example
+import heapq, time, random
+
+def get_pair(i):
+    return (10-random.randint(1, 10), i, random.randint(0, RANGE))
+
+RANGE = 10000000
+
+simple_prio = []
+print("Priority Enqueued 1:9, 2:9, 3:9, 4:5, 5:1")
+heapq.heappush(simple_prio, (9, 1, 1))
+heapq.heappush(simple_prio, (9, 2, 2))
+heapq.heappush(simple_prio, (9, 3, 3))
+heapq.heappush(simple_prio, (5, 4, 4))
+heapq.heappush(simple_prio, (1, 5, 5))
+
+# Dequeue order check
+test_out = []
+while simple_prio:
+    test_out.append(heapq.heappop(simple_prio)[2])
+print("Simple Priority Queue Dequeued:", ' '.join(map(str, test_out)))
+
+# Speed test
+pairs = [get_pair(i) for i in range(RANGE)]
+start_time = time.time()
+for pair in pairs:
+    heapq.heappush(simple_prio, pair)
+elapsed = time.time() - start_time
+print("Simple priority queue enqueue time:", elapsed)
+
+count = 0
+start_time = time.time()
+while simple_prio:
+    heapq.heappop(simple_prio)
+    count += 1
+elapsed = time.time() - start_time
+print("Simple priority queue dequeued items:", count)
+print("Simple priority queue dequeue time:", elapsed)
+print("-----")
